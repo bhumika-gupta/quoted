8c21475542999226664211ea30a0126b68334941
guptab8 <guptab8@mcmaster.ca>
2025-05-13 14:39:37 -0400

    Trial run to scrape quotes from multiple pages

---
+++ main.py
@@ -0,0 +1,53 @@
+import httpx
+from selectolax.parser import HTMLParser
+import sys # access command line arguments
+import json
+
+BOOK_URLS = {
+    "Page 1": "https://quotes.toscrape.com/page/1/",
+    "Page 2": "https://quotes.toscrape.com/page/2/",
+    "Page 3": "https://quotes.toscrape.com/page/3/"
+}
+print("Select a page to scrape:")
+for i, title in enumerate(BOOK_URLS, 1):
+    print(f"{i}. {title}")
+choice = int(input("Enter a number: "))
+url = list(BOOK_URLS.values()   )[choice - 1]
+
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15"}
+
+resp = httpx.get(url, headers=headers)
+html = HTMLParser(resp.text)
+
+quotes_data = []
+books = html.css("div.quote")
+
+for book in books:
+        quote = book.css_first("span.text").text()
+        author = book.css_first("small.author").text()
+        # print(f"{quote} – {author}")
+        quotes_data.append({"quote": quote, "author": author})
+
+# save to JSON
+with open("quotes.json", "w", encoding="utf-8") as f:
+    json.dump(quotes_data, f, indent=2, ensure_ascii=False)
+
+html_output = "<html><body><h1>Quotes</h1><ul>"
+for data in quotes_data: 
+    html_output += f"<li>{data['quote']} – <em>{data['author']}</em></li>"
+html_output += "</ul></body></html>"
+
+with open("quotes.html", "w", encoding="utf-8") as f:
+    f.write(html_output)
+
+print("Quotes saved to 'quotes.json' and 'quotes.html'")
+
+
+"""if __name__ == "__main__":
+    url = sys.argv[1] if len(sys.argv) > 1 else "https://quotes.toscrape.com"
+    results = scrape_quotes(url)
+
+    
+
+    for quote, author in results:
+        print(f"{quote} – {author}")"""
